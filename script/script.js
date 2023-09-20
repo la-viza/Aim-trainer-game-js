717e58665bc0985e8e0bc0e6c7483be1882a2404
la-viza <97736137+la-viza@users.noreply.github.com>
2023-09-20 15:38:24 +0200

    + js

--- script/script.js
+++ script/script.js
@@ -1,6 +1,3 @@
-
-
-
 // let time = 0,
 // unlimited = false,
 // playing = 0,
@@ -8,8 +5,6 @@
 // missed = 0,
 // accuracy = "0%",
 // interval;
-
-
 
 // function createRandomCircle () {
 // if ("playing") {
@@ -20,12 +15,9 @@
 // const size = getRandom(30, 100);
 // const colors =["yellow", "red", "blue", "green"];
 
-
 // const  { width, height} = board.getBoundingClientRect();
 // const x = getRandomNumber(0, width - size);
 // const y = getRandomNumber(0, height - size);
-
-
 
 // circle.classList.add("circle");
 //     circle.style.width = ``
@@ -36,11 +28,43 @@
 //     return Math.random(Math.floor() * (Max - Min) + Min);
 // }
 
+const target = document.querySelector(".circle");
 
-const screen = document.querySelector(".circle");
+function timeSet() {
+  target.style.top = Math.random() * 750 + "px";
+  target.style.left = Math.random() * 750 + "px";
+}
 
-screen.style.top = Math.random() * 750 + "px";
-screen.style.left = Math.random() * 750 + "px";
+setInterval(timeSet, 1000);
+
+target.addEventListener("click", shoot);
+
+let score = 0;
+playerScore = document.querySelector(".score");
+
+let finalScore = document.querySelector("#final-result");
+
+
+
+function shoot(event) {
+score++;
+document.querySelector(".score").innerHTML =  score;
+document.querySelector("#final-result").innerHTML =  score;
+
+console.log(score);  
+}
+
+function countDown() {
+
+
+
+    console.log("It works")
+}
+
+
+
+setTimeout(countDown, (1000 * 30));
+console.log("1 2 3");
 
 
 
