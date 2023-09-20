4e5faf9cfbc04d20ebb59fdee70ac480c78b4aee
la-viza <97736137+la-viza@users.noreply.github.com>
2023-09-20 13:57:01 +0200

    rearranging the code structure

--- script/script.js
+++ script/script.js
@@ -1,26 +1,53 @@
-let board = document.querySelector("#board");
-
-
-let time = 0,
-unlimited = false,
-playing = 0,
-hits = 0,
-missed = 0,
-accuracy = "0%",
-interval;
 
 
 
-function createRandomCircle () {
-if ("playing") {
-    return;
-}
+// let time = 0,
+// unlimited = false,
+// playing = 0,
+// hits = 0,
+// missed = 0,
+// accuracy = "0%",
+// interval;
 
-const circle = document.createElement("div");
-const size = getRandom(30, 100);
-const colors =["yellow", "red", "blue", "green"];
-}
 
-function getRandomNumber(Min, Max) {
-    return Math.random(Math.floor() * (Max - Min) + Min); 
-}
+
+// function createRandomCircle () {
+// if ("playing") {
+//     return;
+// }
+
+// const circle = document.createElement("div");
+// const size = getRandom(30, 100);
+// const colors =["yellow", "red", "blue", "green"];
+
+
+// const  { width, height} = board.getBoundingClientRect();
+// const x = getRandomNumber(0, width - size);
+// const y = getRandomNumber(0, height - size);
+
+
+
+// circle.classList.add("circle");
+//     circle.style.width = ``
+
+// }
+
+// function getRandomNumber(Min, Max) {
+//     return Math.random(Math.floor() * (Max - Min) + Min);
+// }
+
+
+const screen = document.querySelector(".circle");
+
+screen.style.top = Math.random() * 750 + "px";
+screen.style.left = Math.random() * 750 + "px";
+
+
+
+
+
+
+
+
+
+
